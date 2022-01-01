4120b75df673060b49cf950c6d62dd8f67004adf
Yurino Oga <yurino.o@latona.io>
2022-01-01 22:56:35 +0900

    release

---
+++ README.md
@@ -0,0 +1,270 @@
+# sap-api-integrations-outbound-delivery-reads-rmq-kube
+sap-api-integrations-outbound-delivery-reads-rmq-kube は、外部システム(特にエッジコンピューティング環境)をSAPと統合することを目的に、SAP API で 出荷データを取得するマイクロサービスです。      
+sap-api-integrations-outbound-delivery-reads-rmq-kube には、サンプルのAPI Json フォーマットが含まれています。    
+sap-api-integrations-outbound-delivery-reads-rmq-kube は、オンプレミス版である（＝クラウド版ではない）SAPS4HANA API の利用を前提としています。クラウド版APIを利用する場合は、ご注意ください。     
+https://api.sap.com/api/OP_API_OUTBOUND_DELIVERY_SRV_0002/overview  
+## 動作環境  
+sap-api-integrations-outbound-delivery-reads-rmq-kube は、主にエッジコンピューティング環境における動作にフォーカスしています。  
+使用する際は、事前に下記の通り エッジコンピューティングの動作環境（推奨/必須）を用意してください。  
+・ エッジ Kubernetes （推奨）    
+・ AION のリソース （推奨)    
+・ OS: LinuxOS （必須）    
+・ CPU: ARM/AMD/Intel（いずれか必須）  
+・ RabbitMQ on Kubernetes  
+・ RabbitMQ Client  
+
+## クラウド環境での利用
+sap-api-integrations-outbound-delivery-reads-rmq-kube は、外部システムがクラウド環境である場合にSAPと統合するときにおいても、利用可能なように設計されています。  
+
+## RabbitMQ への JSON Output
+
+sap-api-integrations-outbound-delivery-reads-rmq-kube は、Outputとして、RabbitMQ へのメッセージをJSON形式で出力します。
+
+## RabbitMQ の マスタサーバ環境
+
+sap-api-integrations-outbound-delivery-reads-rmq-kube が利用する RabbitMQ のマスタサーバ環境は、[rabbitmq-on-kubernetes](https://github.com/latonaio/rabbitmq-on-kubernetes) です。  
+当該マスタサーバ環境は、同じエッジコンピューティングデバイスに配置されても、別の物理(仮想)サーバ内に配置されても、どちらでも構いません。
+
+## RabbitMQ の Golang Runtime ライブラリ
+sap-api-integrations-outbound-delivery-reads-rmq-kube は、RabbitMQ の Golang Runtime ライブラリ として、[rabbitmq-golang-client](https://github.com/latonaio/rabbitmq-golang-client)を利用しています。
+
+## デプロイ・稼働
+sap-api-integrations-outbound-delivery-reads-rmq-kube の デプロイ・稼働 を行うためには、aion-service-definitions の services.yml に、本レポジトリの services.yml を設定する必要があります。
+
+kubectl apply - f 等で Deployment作成後、以下のコマンドで Pod が正しく生成されていることを確認してください。
+```
+$ kubectl get pods
+```
+
+## 本レポジトリ が 対応する API サービス
+sap-api-integrations-outbound-delivery-reads-rmq-kube が対応する APIサービス は、次のものです。
+
+* APIサービス概要説明 URL: https://api.sap.com/api/OP_API_OUTBOUND_DELIVERY_SRV_0002/overview  
+* APIサービス名(=baseURL): API_OUTBOUND_DELIVERY_SRV;v=0002
+
+## 本レポジトリ に 含まれる API名
+sap-api-integrations-outbound-delivery-reads-rmq-kube には、次の API をコールするためのリソースが含まれています。  
+
+* A_OutbDeliveryHeader（出荷伝票 - ヘッダ）
+* ToHeaderPartner（出荷伝票 - 取引先機能 ※To）  
+* ToPartnerAddress（出荷伝票 - 取引先アドレス ※To）
+* ToItem（出荷伝票 - 明細 ※To）
+* ToItemDocumentFlow（出荷伝票 - 明細伝票フロー ※To）
+* A_OutbDeliveryHeader('{DeliveryDocument}')/to_DeliveryDocumentPartner（出荷伝票 - 取引先機能）
+* ToPartnerAddress（出荷伝票 - 取引先アドレス ※To）
+* A_OutbDeliveryPartner(PartnerFunction='{PartnerFunction}',SDDocument='{SDDocument}')/to_Address2（出荷伝票 - 取引先アドレス）
+* A_OutbDeliveryItem（出荷伝票 - 明細）
+
+## API への 値入力条件 の 初期値
+sap-api-integrations-outbound-delivery-reads において、API への値入力条件の初期値は、入力ファイルレイアウトの種別毎に、次の通りとなっています。   
+
+### SDC レイアウト
+
+* inoutSDC.OutboundDelivery.DeliveryDocument（出荷伝票）
+* inoutSDC.OutboundDelivery.HeaderPartner.SDDocument（販売伝票 ※出荷伝票の取引先機能関連のAPIをコールするときに出荷伝票ではなく販売伝票としての項目値が必要です。通常は、出荷伝票の値＝販売伝票の値、となります）  
+* inoutSDC.OutboundDelivery.HeaderPartner.PartnerFunction（取引先機能）
+* inoutSDC.OutboundDelivery.DeliveryDocumentItem.DeliveryDocumentItem（出荷伝票明細）  
+
+## SAP API Bussiness Hub の API の選択的コール
+
+Latona および AION の SAP 関連リソースでは、Inputs フォルダ下の sample.json の accepter に取得したいデータの種別（＝APIの種別）を入力し、指定することができます。  
+なお、同 accepter にAll(もしくは空白)の値を入力することで、全データ（＝全APIの種別）をまとめて取得することができます。  
+
+* sample.jsonの記載例(1)  
+
+accepter において 下記の例のように、データの種別（＝APIの種別）を指定します。  
+ここでは、"Header" が指定されています。    
+  
+```
+	"api_schema": "sap.s4.beh.outbounddelivery.v1.OutboundDelivery.Created.v1",
+	"accepter": ["Header"],
+	"delivery_document": "80000000",
+	"deleted": false
+```
+  
+* 全データを取得する際のsample.jsonの記載例(2)  
+
+全データを取得する場合、sample.json は以下のように記載します。  
+
+```
+	"api_schema": "sap.s4.beh.outbounddelivery.v1.OutboundDelivery.Created.v1",
+	"accepter": ["All"],
+	"delivery_document": "80000000",
+	"deleted": false
+```
+
+## 指定されたデータ種別のコール
+
+accepter における データ種別 の指定に基づいて SAP_API_Caller 内の caller.go で API がコールされます。  
+caller.go の func() 毎 の 以下の箇所が、指定された API をコールするソースコードです。  
+
+```
+func (c *SAPAPICaller) AsyncGetOutboundDelivery(deliveryDocument, sDDocument, partnerFunction, deliveryDocumentItem, precedingDocument, precedingDocumentItem string, accepter []string) {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(accepter))
+	for _, fn := range accepter {
+		switch fn {
+		case "Header":
+			func() {
+				c.Header(deliveryDocument)
+				wg.Done()
+			}()
+		case "HeaderPartner":
+			func() {
+				c.HeaderPartner(sDDocument, partnerFunction)
+				wg.Done()
+			}()
+		case "PartnerAddress":
+			func() {
+				c.PartnerAddress(partnerFunction, sDDocument)
+				wg.Done()
+			}()
+		case "Item":
+			func() {
+				c.Item(deliveryDocument, deliveryDocumentItem)
+				wg.Done()
+			}()
+		default:
+			wg.Done()
+		}
+	}
+
+	wg.Wait()
+}
+```
+
+## SAP API Business Hub における API サービス の バージョン と バージョン におけるデータレイアウトの相違
+
+SAP API Business Hub における API サービス のうちの 殆どの API サービス の バージョンは、v1.X.X であり、そのため殆どの API サービス 間 の データレイアウトは統一されています。
+従って、Latona および AION における リソースにおいても、これらの 揃った v1.X.X のバージョンに従い、データレイアウトが統一されています。
+一方、本レポジトリ に関わる API である Outbound Delivery のサービスは、v2.X.X までバージョンが進んでおり、その結果、本レポジトリ内の一部のAPIのデータレイアウトが、他のAPIサービスのものと異なっています。
+
+* v1.X.X のバージョンである API サービス の データレイアウト（=responses）  
+v1.X.X のバージョンであるAPIサービスのデータレイアウト（=responses）は、例えば、次の通りです。
+
+```
+type PartnerFunction struct {
+	D struct {
+		Results []struct {
+			Metadata struct {
+				ID   string `json:"id"`
+				URI  string `json:"uri"`
+				Type string `json:"type"`
+			} `json:"__metadata"`
+			SDDocument           string `json:"SDDocument"`
+			PartnerFunction      string `json:"PartnerFunction"`
+			Customer             string `json:"Customer"`
+			Supplier             string `json:"Supplier"`
+		} `json:"results"`
+	} `json:"d"`
+}
+
+
+```
+
+* 本レポジトリ内の一部のAPIのデータレイアウトを含む、v2.X.X のバージョンである API サービス の データレイアウト（=responses）  
+本レポジトリ内の一部のAPIのデータレイアウトを含む、v2.X.X のバージョンである API サービスのデータレイアウト（=responses）は、次の通りです。  
+ここで言う本レポジトリ内の一部のAPIのデータレイアウトには、PartnerAddressが含まれ、  
+本レポジトリ内の他のデータレイアウトは、v1.X.X と同様のデータレイアウトとなっています。
+（つまり、本レポジトリ内においても、現時点でv2.X.X のデータレイアウトを踏襲しているのは、PartnerAddress のみで、Header、PartnerFunction、Item、などのAPIでは、v1.X.X と同様のデータレイアウトとなっています）  
+
+```
+type PartnerAddress struct {
+	D *struct {
+			Metadata struct {
+				ID   string `json:"id"`
+				URI  string `json:"uri"`
+				Type string `json:"type"`
+			    Etag string `json:"etag"`
+			} `json:"__metadata"`
+			AddressID              string `json:"AddressID"`
+			BusinessPartnerName1   string `json:"BusinessPartnerName1"`
+			Country                string `json:"Country"`
+			Region                 string `json:"Region"`
+			StreetName             string `json:"StreetName"`
+			CityName               string `json:"CityName"`
+			PostalCode             string `json:"PostalCode"`
+			CorrespondenceLanguage string `json:"CorrespondenceLanguage"`
+			FaxNumber              string `json:"FaxNumber"`
+			PhoneNumber            string `json:"PhoneNumber"`
+	} `json:"d"`
+}
+```
+
+このように、v1.X.X のバージョンである API サービス の データレイアウトと、v2.X.X のバージョンである API サービス の データレイアウトは、Results の配列構造を持っているか持っていないかという点が異なります。  
+
+## Output  
+本マイクロサービスでは、[golang-logging-library](https://github.com/latonaio/golang-logging-library) により、以下のようなデータがJSON形式で出力されます。  
+以下の sample.json の例は、SAP 出荷伝票 の ヘッダデータ が取得された結果の JSON の例です。  
+以下の項目のうち、"DeliveryDocument" ～ "to_DeliveryDocumentItem" は、/SAP_API_Output_Formatter/type.go 内 の Type Header {} による出力結果です。"cursor" ～ "time"は、golang-logging-library による 定型フォーマットの出力結果です。  
+
+```
+{
+	"cursor": "/Users/latona2/bitbucket/sap-api-integrations-outbound-delivery-reads/SAP_API_Caller/caller.go#L73",
+	"function": "sap-api-integrations-outbound-delivery-reads/SAP_API_Caller.(*SAPAPICaller).Header",
+	"level": "INFO",
+	"message": [
+		{
+			"DeliveryDocument": "80000000",
+			"DeliveryDocumentType": "LF",
+			"DocumentDate": "/Date(1471392000000)/",
+			"ActualGoodsMovementDate": "/Date(1471392000000)/",
+			"ActualDeliveryRoute": "TR0002",
+			"Shippinglocationtimezone": "PST",
+			"Receivinglocationtimezone": "EST",
+			"ActualGoodsMovementTime": "PT07H00M00S",
+			"BillingDocumentDate": "/Date(1471392000000)/",
+			"CompleteDeliveryIsDefined": false,
+			"ConfirmationTime": "PT00H00M00S",
+			"CreationDate": "/Date(1471392000000)/",
+			"CreationTime": "PT08H21M06S",
+			"CustomerGroup": "01",
+			"DeliveryBlockReason": "",
+			"DeliveryDate": "/Date(1471564800000)/",
+			"DeliveryDocumentBySupplier": "",
+			"DeliveryIsInPlant": false,
+			"DeliveryPriority": "00",
+			"DeliveryTime": "PT04H00M00S",
+			"GoodsIssueOrReceiptSlipNumber": "",
+			"GoodsIssueTime": "PT07H00M00S",
+			"HeaderBillingBlockReason": "",
+			"HeaderGrossWeight": "3.000",
+			"HeaderNetWeight": "2.700",
+			"HeaderVolume": "0.000",
+			"HeaderVolumeUnit": "",
+			"HeaderWeightUnit": "G",
+			"IncotermsClassification": "EXW",
+			"IsExportDelivery": "",
+			"LastChangeDate": "",
+			"LoadingDate": "/Date(1471392000000)/",
+			"LoadingPoint": "",
+			"LoadingTime": "PT07H00M00S",
+			"MeansOfTransport": "",
+			"OrderCombinationIsAllowed": false,
+			"OrderID": "",
+			"OverallDelivConfStatus": "",
+			"OverallDelivReltdBillgStatus": "C",
+			"OverallGoodsMovementStatus": "C",
+			"OverallPackingStatus": "",
+			"OverallPickingConfStatus": "",
+			"OverallPickingStatus": "C",
+			"PickingDate": "/Date(1471305600000)/",
+			"PickingTime": "PT07H00M00S",
+			"PlannedGoodsIssueDate": "/Date(1471392000000)/",
+			"ReceivingPlant": "",
+			"ShippingCondition": "01",
+			"ShippingPoint": "1710",
+			"ShippingType": "",
+			"ShipToParty": "17100001",
+			"SoldToParty": "17100001",
+			"Supplier": "",
+			"TransportationGroup": "0001",
+			"TransportationPlanningDate": "/Date(1471392000000)/",
+			"TransportationPlanningTime": "PT07H00M00S",
+			"to_DeliveryDocumentPartner": "https://sandbox.api.sap.com/s4hanacloud/sap/opu/odata/sap/API_OUTBOUND_DELIVERY_SRV;v=0002/A_OutbDeliveryHeader('80000000')/to_DeliveryDocumentPartner",
+			"to_DeliveryDocumentItem": "https://sandbox.api.sap.com/s4hanacloud/sap/opu/odata/sap/API_OUTBOUND_DELIVERY_SRV;v=0002/A_OutbDeliveryHeader('80000000')/to_DeliveryDocumentItem"
+		}
+	],
+	"time": "2021-12-31T18:04:17.677966+09:00"
+}
+```
---
+++ SAP_API_Caller/caller.go
@@ -0,0 +1,417 @@
+package sap_api_caller
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	sap_api_output_formatter "sap-api-integrations-outbound-delivery-reads-rmq-kube/SAP_API_Output_Formatter"
+	"strings"
+	"sync"
+
+	"github.com/latonaio/golang-logging-library/logger"
+	"golang.org/x/xerrors"
+)
+
+type RMQOutputter interface {
+	Send(sendQueue string, payload map[string]interface{}) error
+}
+
+type SAPAPICaller struct {
+	baseURL      string
+	apiKey       string
+	outputQueues []string
+	outputter    RMQOutputter
+	log          *logger.Logger
+}
+
+func NewSAPAPICaller(baseUrl string, outputQueueTo []string, outputter RMQOutputter, l *logger.Logger) *SAPAPICaller {
+	return &SAPAPICaller{
+		baseURL:      baseUrl,
+		apiKey:       GetApiKey(),
+		outputQueues: outputQueueTo,
+		outputter:    outputter,
+		log:          l,
+	}
+}
+
+func (c *SAPAPICaller) AsyncGetOutboundDelivery(deliveryDocument, sDDocument, partnerFunction, deliveryDocumentItem string, accepter []string) {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(accepter))
+	for _, fn := range accepter {
+		switch fn {
+		case "Header":
+			func() {
+				c.Header(deliveryDocument)
+				wg.Done()
+			}()
+		case "HeaderPartner":
+			func() {
+				c.HeaderPartner(sDDocument, partnerFunction)
+				wg.Done()
+			}()
+		case "PartnerAddress":
+			func() {
+				c.PartnerAddress(partnerFunction, sDDocument)
+				wg.Done()
+			}()
+		case "Item":
+			func() {
+				c.Item(deliveryDocument, deliveryDocumentItem)
+				wg.Done()
+			}()
+		default:
+			wg.Done()
+		}
+	}
+
+	wg.Wait()
+}
+
+func (c *SAPAPICaller) Header(deliveryDocument string) {
+	headerData, err := c.callOutboundDeliverySrvAPIRequirementHeader("A_OutbDeliveryHeader", deliveryDocument)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	err = c.outputter.Send(c.outputQueues[0], map[string]interface{}{"message": headerData, "function": "OutboundDeliveryHeader"})
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(headerData)
+
+	headerPartnerData, err := c.callToHeaderPartner(headerData[0].ToHeaderPartner)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	err = c.outputter.Send(c.outputQueues[0], map[string]interface{}{"message": headerPartnerData, "function": "OutboundDeliveryHeaderPartner"})
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(headerPartnerData)
+	
+	partnerAddressData, err := c.callToPartnerAddress(headerPartnerData[0].ToPartnerAddress)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	err = c.outputter.Send(c.outputQueues[0], map[string]interface{}{"message": partnerAddressData, "function": "OutboundDeliveryPartnerAddress"})
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(partnerAddressData)
+	
+	itemData, err := c.callToItem(headerData[0].ToItem)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	err = c.outputter.Send(c.outputQueues[0], map[string]interface{}{"message": itemData, "function": "OutboundDeliveryItem"})
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(itemData)
+	
+	itemDocumentFlowData, err := c.callToItemDocumentFlow(itemData[0].ToItemDocumentFlow)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	err = c.outputter.Send(c.outputQueues[0], map[string]interface{}{"message": itemDocumentFlowData, "function": "OutboundDeliveryItemDocumentFlow"})
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(itemDocumentFlowData)
+}
+
+func (c *SAPAPICaller) callOutboundDeliverySrvAPIRequirementHeader(api, deliveryDocument string) ([]sap_api_output_formatter.Header, error) {
+	url := strings.Join([]string{c.baseURL, "API_OUTBOUND_DELIVERY_SRV;v=0002", api}, "/")
+	req, _ := http.NewRequest("GET", url, nil)
+
+	c.setHeaderAPIKeyAccept(req)
+	c.getQueryWithHeader(req, deliveryDocument)
+
+	resp, err := new(http.Client).Do(req)
+	if err != nil {
+		return nil, xerrors.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToHeader(byteArray, c.log)
+	if err != nil {
+		return nil, xerrors.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToHeaderPartner(url string) ([]sap_api_output_formatter.ToHeaderPartner, error) {
+	req, _ := http.NewRequest("GET", url, nil)
+	c.setHeaderAPIKeyAccept(req)
+
+	resp, err := new(http.Client).Do(req)
+	if err != nil {
+		return nil, xerrors.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToToHeaderPartner(byteArray, c.log)
+	if err != nil {
+		return nil, xerrors.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToPartnerAddress(url string) (*sap_api_output_formatter.ToPartnerAddress, error) {
+	req, _ := http.NewRequest("GET", url, nil)
+	c.setHeaderAPIKeyAccept(req)
+
+	resp, err := new(http.Client).Do(req)
+	if err != nil {
+		return nil, xerrors.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToToPartnerAddress(byteArray, c.log)
+	if err != nil {
+		return nil, xerrors.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToItem(url string) ([]sap_api_output_formatter.ToItem, error) {
+	req, _ := http.NewRequest("GET", url, nil)
+	c.setHeaderAPIKeyAccept(req)
+
+	resp, err := new(http.Client).Do(req)
+	if err != nil {
+		return nil, xerrors.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToToItem(byteArray, c.log)
+	if err != nil {
+		return nil, xerrors.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToItemDocumentFlow(url string) ([]sap_api_output_formatter.ToItemDocumentFlow, error) {
+	req, _ := http.NewRequest("GET", url, nil)
+	c.setHeaderAPIKeyAccept(req)
+
+	resp, err := new(http.Client).Do(req)
+	if err != nil {
+		return nil, xerrors.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToToItemDocumentFlow(byteArray, c.log)
+	if err != nil {
+		return nil, xerrors.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) HeaderPartner(sDDocument, partnerFunction string) {
+	headerPartnerData, err := c.callOutboundDeliverySrvAPIRequirementHeaderPartner(fmt.Sprintf("A_OutbDeliveryHeader('%s')/to_DeliveryDocumentPartner", sDDocument), sDDocument, partnerFunction)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	err = c.outputter.Send(c.outputQueues[0], map[string]interface{}{"message": headerPartnerData, "function": "OutboundDeliveryHeaderPartner"})
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(headerPartnerData)
+
+	partnerAddressData, err := c.callToPartnerAddress2(headerPartnerData[0].ToPartnerAddress)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	err = c.outputter.Send(c.outputQueues[0], map[string]interface{}{"message": partnerAddressData, "function": "OutboundDeliveryPartnerAddress"})
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(partnerAddressData)
+}
+
+func (c *SAPAPICaller) callOutboundDeliverySrvAPIRequirementHeaderPartner(api, sDDocument, partnerFunction string) ([]sap_api_output_formatter.HeaderPartner, error) {
+	url := strings.Join([]string{c.baseURL, "API_OUTBOUND_DELIVERY_SRV;v=0002", api}, "/")
+	req, _ := http.NewRequest("GET", url, nil)
+
+	c.setHeaderAPIKeyAccept(req)
+	c.getQueryWithHeaderPartner(req, sDDocument, partnerFunction)
+
+	resp, err := new(http.Client).Do(req)
+	if err != nil {
+		return nil, xerrors.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToHeaderPartner(byteArray, c.log)
+	if err != nil {
+		return nil, xerrors.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToPartnerAddress2(url string) (*sap_api_output_formatter.ToPartnerAddress, error) {
+	req, _ := http.NewRequest("GET", url, nil)
+	c.setHeaderAPIKeyAccept(req)
+
+	resp, err := new(http.Client).Do(req)
+	if err != nil {
+		return nil, xerrors.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToToPartnerAddress(byteArray, c.log)
+	if err != nil {
+		return nil, xerrors.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) PartnerAddress(partnerFunction, sDDocument string) {
+	data, err := c.callOutboundDeliverySrvAPIRequirementPartnerAddress(fmt.Sprintf("A_OutbDeliveryPartner(PartnerFunction='%s',SDDocument='%s')/to_Address2", partnerFunction, sDDocument), partnerFunction, sDDocument)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	err = c.outputter.Send(c.outputQueues[0], map[string]interface{}{"message": data, "function": "OutboundDeliveryPartnerAddress"})
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(data)
+}
+
+func (c *SAPAPICaller) callOutboundDeliverySrvAPIRequirementPartnerAddress(api, partnerFunction, sDDocument string) (*sap_api_output_formatter.PartnerAddress, error) {
+	url := strings.Join([]string{c.baseURL, "API_OUTBOUND_DELIVERY_SRV;v=0002", api}, "/")
+	req, _ := http.NewRequest("GET", url, nil)
+
+	c.setHeaderAPIKeyAccept(req)
+	//	c.getQueryWithPartnerAddress(req, partnerFunction, sDDocument)
+
+	resp, err := new(http.Client).Do(req)
+	if err != nil {
+		return nil, xerrors.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToPartnerAddress(byteArray, c.log)
+	if err != nil {
+		return nil, xerrors.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) Item(deliveryDocument, deliveryDocumentItem string) {
+	itemData, err := c.callOutboundDeliverySrvAPIRequirementItem("A_OutbDeliveryItem", deliveryDocument, deliveryDocumentItem)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	err = c.outputter.Send(c.outputQueues[0], map[string]interface{}{"message": itemData, "function": "OutboundDeliveryItem"})
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(itemData)
+
+	itemDocumentFlowData, err := c.callToItemDocumentFlow2(itemData[0].ToItemDocumentFlow)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	err = c.outputter.Send(c.outputQueues[0], map[string]interface{}{"message": itemDocumentFlowData, "function": "OutboundDeliveryItemDocumentFlow"})
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(itemDocumentFlowData)
+}
+
+func (c *SAPAPICaller) callOutboundDeliverySrvAPIRequirementItem(api, deliveryDocument, deliveryDocumentItem string) ([]sap_api_output_formatter.Item, error) {
+	url := strings.Join([]string{c.baseURL, "API_OUTBOUND_DELIVERY_SRV;v=0002", api}, "/")
+	req, _ := http.NewRequest("GET", url, nil)
+
+	c.setHeaderAPIKeyAccept(req)
+	c.getQueryWithItem(req, deliveryDocument, deliveryDocumentItem)
+
+	resp, err := new(http.Client).Do(req)
+	if err != nil {
+		return nil, xerrors.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToItem(byteArray, c.log)
+	if err != nil {
+		return nil, xerrors.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToItemDocumentFlow2(url string) ([]sap_api_output_formatter.ToItemDocumentFlow, error) {
+	req, _ := http.NewRequest("GET", url, nil)
+	c.setHeaderAPIKeyAccept(req)
+
+	resp, err := new(http.Client).Do(req)
+	if err != nil {
+		return nil, xerrors.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToToItemDocumentFlow(byteArray, c.log)
+	if err != nil {
+		return nil, xerrors.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) setHeaderAPIKeyAccept(req *http.Request) {
+	req.Header.Set("APIKey", c.apiKey)
+	req.Header.Set("Accept", "application/json")
+}
+
+func (c *SAPAPICaller) getQueryWithHeader(req *http.Request, deliveryDocument string) {
+	params := req.URL.Query()
+	params.Add("$filter", fmt.Sprintf("DeliveryDocument eq '%s'", deliveryDocument))
+	req.URL.RawQuery = params.Encode()
+}
+
+func (c *SAPAPICaller) getQueryWithHeaderPartner(req *http.Request, sDDocument, partnerFunction string) {
+	params := req.URL.Query()
+	params.Add("$filter", fmt.Sprintf("SDDocument eq '%s' and PartnerFunction eq '%s'", sDDocument, partnerFunction))
+	req.URL.RawQuery = params.Encode()
+}
+
+//func (c *SAPAPICaller) getQueryWithPartnerAddress(req *http.Request, partnerFunction, sDDocument string) {
+//	params := req.URL.Query()
+//	params.Add("$filter", fmt.Sprintf("PartnerFunction eq '%s' and SDDocument eq '%s'", partnerFunction, sDDocument))
+//	req.URL.RawQuery = params.Encode()
+//}
+
+func (c *SAPAPICaller) getQueryWithItem(req *http.Request, deliveryDocument, deliveryDocumentItem string) {
+	params := req.URL.Query()
+	params.Add("$filter", fmt.Sprintf("DeliveryDocument eq '%s' and DeliveryDocumentItem eq '%s'", deliveryDocument, deliveryDocumentItem))
+	req.URL.RawQuery = params.Encode()
+}
